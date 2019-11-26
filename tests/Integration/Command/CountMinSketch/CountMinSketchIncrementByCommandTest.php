50c283e7f715f7d0c9a5031f38e0b4922d7be1a4
BeWater <rafa.campoy@gmail.com>
2019-11-26 16:01:56 +0100

    fixed header @link and some small refactoring

--- tests/Integration/Command/CountMinSketch/CountMinSketchIncrementByCommandTest.php
+++ tests/Integration/Command/CountMinSketch/CountMinSketchIncrementByCommandTest.php
@@ -4,7 +4,7 @@
  * @author    Rafael Campoy <rafa.campoy@gmail.com>
  * @copyright 2019 Rafael Campoy <rafa.campoy@gmail.com>
  * @license   MIT
- * @link      https://github.com/averias/php-rejson
+ * @link      https://github.com/averias/phpredis-bloom
  *
  * Copyright and license information, is included in
  * the LICENSE file that is distributed with this source code.
@@ -12,6 +12,7 @@
 
 namespace Averias\RedisBloom\Tests\Integration\Command\CountMinSketch;
 
+use Averias\RedisBloom\Enum\Keys;
 use Averias\RedisBloom\Exception\ResponseException;
 use Averias\RedisBloom\Tests\BaseTestIntegration;
 
@@ -19,19 +20,27 @@ class CountMinSketchIncrementByCommandTest extends BaseTestIntegration
 {
     public function testIncrementSuccessfully(): void
     {
-        static::$reBloomClient->countMinSketchInitByDim('key-incr1', 4, 4);
-        $result = static::$reBloomClient->countMinSketchIncrementBy('key-incr1', 'green', 40, 'black', 90, 'orange', 6);
+        static::$reBloomClient->countMinSketchInitByDim(Keys::INCREMENT_BY_1, 4, 4);
+        $result = static::$reBloomClient->countMinSketchIncrementBy(
+            Keys::INCREMENT_BY_1,
+            'green',
+            40,
+            'black',
+            90,
+            'orange',
+            6
+        );
         $this->assertTrue($result);
 
-        $query = static::$reBloomClient->countMinSketchQuery('key-incr1', 'green', 'black', 'orange');
+        $query = static::$reBloomClient->countMinSketchQuery(Keys::INCREMENT_BY_1, 'green', 'black', 'orange');
         $this->assertEquals(40, $query[0]);
         $this->assertEquals(90, $query[1]);
         $this->assertEquals(6, $query[2]);
 
-        $result = static::$reBloomClient->countMinSketchIncrementBy('key-incr1', 12, 31, 13.4, 32);
+        $result = static::$reBloomClient->countMinSketchIncrementBy(Keys::INCREMENT_BY_1, 12, 31, 13.4, 32);
         $this->assertTrue($result);
 
-        $query = static::$reBloomClient->countMinSketchQuery('key-incr1', 12, 13.4);
+        $query = static::$reBloomClient->countMinSketchQuery(Keys::INCREMENT_BY_1, 12, 13.4);
         $this->assertEquals(31, $query[0]);
         $this->assertEquals(32, $query[1]);
     }
@@ -44,19 +53,19 @@ class CountMinSketchIncrementByCommandTest extends BaseTestIntegration
     public function testIncrementException($key, $arguments): void
     {
         $this->expectException(ResponseException::class);
-        static::$reBloomClient->countMinSketchInitByDim('key-incr2', 4, 4);
+        static::$reBloomClient->countMinSketchInitByDim(Keys::INCREMENT_BY_2, 4, 4);
         static::$reBloomClient->countMinSketchIncrementBy($key, ...$arguments);
     }
 
     public function getExceptionDataProvider(): array
     {
         return [
-            ['key-incr2', [true, 44]],
-            ['key-incr2', ['bar', true]],
-            ['key-incr2', ['foo', 4, 'bar']],
-            ['key-incr2', ['foo', 4.3]],
-            ['key-incr2', ['baz', true]],
-            ['key-incr3', ['baz', 10]] // non-existent key
+            [Keys::INCREMENT_BY_2, [true, 44]],
+            [Keys::INCREMENT_BY_2, ['bar', true]],
+            [Keys::INCREMENT_BY_2, ['foo', 4, 'bar']],
+            [Keys::INCREMENT_BY_2, ['foo', 4.3]],
+            [Keys::INCREMENT_BY_2, ['baz', true]],
+            [Keys::INCREMENT_BY_2, ['baz', 10]] // non-existent key
         ];
     }
 }
